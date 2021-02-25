0d18d882c9d547fe296bc3dabb2689d92e5dc0f8
vishnukrishna <7413047-vishnu553@users.noreply.gitlab.com>
2021-02-25 13:07:18 +0530

    Add project files.

---
+++ DTO/Create.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestPrograms.DTO
+{
+    class Create
+    {
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public long Id { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+
+    }
+}
---
+++ RestSetUp.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestPrograms
+{
+   public class RestSetUp
+    {
+        public ListUsersDto GetUsers()
+        {
+            var restClient = new RestClient("https://reqres.in/");
+            var restRequest = new RestRequest("/api/users?page=2",Method.GET);
+            restRequest.AddHeader("Accept", "application/json");
+            IRestResponse response = restClient.Execute(restRequest);
+            var content = response.Content;
+            var users = JsonConvert.DeserializeObject<ListUsersDto>(content);
+            return users;
+
+        }
+    }
+}
